06d9acc5c69f986c843d9cd8eb2051dce640c315
rossman-x <rami-ossma@live.com>
2022-04-29 22:38:30 +0200

    Creating models for CryptoCurrency, DigitalProduct, Nft, Product

---
+++ src/Models/DigitalProduct.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Rami Osman on 29/04/2022.
+//
+#include <iostream>
+#include "DigitalProduct.h"
+
+DigitalProduct::DigitalProduct(string name, string createdAt, string updatedAt, string providerName): Product(name, createdAt, updatedAt, providerName){
+
+}
+
+void DigitalProduct::setToken(string token) {
+    this->token = token;
+}
+
+string DigitalProduct::getToken() {
+    return this->token;
+}
